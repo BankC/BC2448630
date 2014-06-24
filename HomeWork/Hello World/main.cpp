7502d78e950c428996fbc7652c057b482cf09255
Bank Conway <bconway@student.rcc.edu>
2014-06-24 15:24:54 -0700

    Signed-off-by: Bank Conway <bconway@student.rcc.edu>

---
+++ HomeWork/Hello World/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:  Hello World
+ * Author: Bank Conway
+ * Created on June 23, 2014, 4:35 PM
+ * Purpose: First program
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//Execution Begin!
+int main(int argc, char** argv) {
+    std::cout <<"Hello World hahaha"<< endl;
+    return 0;
+}
+
