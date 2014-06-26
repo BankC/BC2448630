8acd897320de9694f08b66d07c20608f3d122617
BankC <bconway@student.rcc.edu>
2014-06-26 12:44:50 -0700

    Signed-off-by: BankC <bconway@student.rcc.edu>

---
+++ HomeWork/Assignment 1/Savitch_8thEdition_Chap1_Prob7/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File: Chapter 1 Problem 7
+ * Author: Bank Conway
+ * Created on June 26, 2014, 12:28 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    cout <<"*************************************************************\n";
+    cout <<"    CCCC                 SSSS              !!\n";
+    cout <<"   C    C              S      S            !!\n";      
+    cout <<"  C                   S                    !!\n";      
+    cout <<" C                    S                    !!\n";
+    cout <<" C                      SSSS               !!\n";  
+    cout <<" C                           S             !!\n";     
+    cout <<"  C                           S            !!\n";          
+    cout <<"   C    C             S      S               \n";  
+    cout <<"    CCCC                SSSS               00\n"; 
+    cout <<"************************************************************\n";
+    cout <<"Computer Science is Cool Stuff!! And this is tedious.";
+    
+            return 0;
+}
+
