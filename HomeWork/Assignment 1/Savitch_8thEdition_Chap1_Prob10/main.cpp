13abf41121ada6280a8322d5eeeeeb785d0313e3
BankC <bconway@student.rcc.edu>
2014-06-26 12:56:33 -0700

    Signed-off-by: BankC <bconway@student.rcc.edu>

---
+++ HomeWork/Assignment 1/Savitch_8thEdition_Chap1_Prob10/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File: Chap 1 problem 10
+ * Author: Bank Conway
+ * Created on June 26, 2014, 12:45 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+    char lter;
+    
+    cout << "Enter a letter and press enter\n";
+    cin >> lter;
+    
+    cout <<"     "<<lter<<lter<<lter<<endl;
+    cout <<"   "<<lter<<"     "<<lter<<endl;
+    cout <<"  "<<lter<<endl;
+    cout <<"  "<<lter<<endl;
+    cout <<"  "<<lter<<endl;
+    cout <<"  "<<lter<<endl;
+    cout <<"  "<<lter<<endl;
+    cout <<"   "<<lter<<"     "<<lter<<endl;
+    cout <<"     "<<lter<<lter<<lter<<endl;
+    
+    
+    return 0;
+}
+
