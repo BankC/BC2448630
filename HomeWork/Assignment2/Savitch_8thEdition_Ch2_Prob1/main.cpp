b8017a77f64b174d73d547bdc8c3fdf7297bfed8
BankC <bconway@student.rcc.edu>
2014-06-30 12:46:13 -0700

    Signed-off-by: BankC <bconway@student.rcc.edu>

--- HomeWork/Assignment2/Savitch_8thEdition_Ch2_Prob1/main.cpp
+++ HomeWork/Assignment2/Savitch_8thEdition_Ch2_Prob1/main.cpp
@@ -4,36 +4,46 @@
  * Created on June 28, 2014, 4:21 PM
  */
 
+//Library System
 #include <iostream>
-
 using namespace std;
 
-
+//Execution
 int main() 
 {
     //Declare variable
     float mTon, CrOunces, CrMTn, Box; //CrMTn is Cereal weight in Metric Ton, 
-    int count_down = 1, ch;
+    char RnAgn;
     
-    //Execution
-   if(count_down > 0)
-   { cout <<"Please Enter the weight of cereal package in ounces.\n"; //Prompt
+    cout <<"Please Enter the weight of cereal package in ounces.\n"; //Prompt
     cin >> CrOunces;
     CrMTn = (CrOunces/3.527392e4);
     Box = 1/CrMTn;
     cout << "There are " << CrMTn <<" Metric Ton of Cereal"<< endl;
     cout <<"So we need " << Box << " boxes of cereal to yield 1 Metric Tone.\n";
     cout << "Would you like to continue?(y/n)\n";
-    cin >> ch;
-    if(ch = 5){count_down ++;}
-    else {return 0;}}
+    cin >> RnAgn ;
+    
+    if(RnAgn != 'y')
+    {
+        cout <<"Have a nice day!\n";
+        return 0;
+    }
+
+    else
+    {
+        cout <<"Please Enter the weight of cereal package in ounces.\n"; 
+        cin >> CrOunces;
+        CrMTn = (CrOunces/3.527392e4);
+        Box = 1/CrMTn;
+         cout << "There are " << CrMTn <<" Metric Ton of Cereal"<< endl;
+        cout <<"So we need " << Box << " boxes of cereal to yield 1 Metric Tone.\n";
+        cout << "Would you like to continue?(y/n)\n";
+        cin >> RnAgn ;
+    }
+    
    
-   
-   else {return 0;}               
-        
             
       
- 
+return 0;}
     
- return 0;}
-
